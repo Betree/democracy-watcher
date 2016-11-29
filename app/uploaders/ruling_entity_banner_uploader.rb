1472c5a8e014e2cceb3a39d14b04e709e562fe78
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2016-11-29 11:38:27 +1100

    Config carrierwave for AWS

--- app/uploaders/ruling_entity_banner_uploader.rb
+++ app/uploaders/ruling_entity_banner_uploader.rb
@@ -3,8 +3,6 @@
 class RulingEntityBannerUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :fog
-
   version :thumb do
     process resize_to_fit: [400, 180]
   end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,20 @@
 CarrierWave.configure do |config|
   if ENV['RACK_ENV'] == 'heroku'
+    config.root = Rails.root.join('tmp')
+    config.cache_dir = 'carrierwave'
+
+
     config.fog_credentials = {
         provider:              'AWS',
         aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        region: ENV['S3_REGION']
     }
-    config.fog_directory  = 'uploads'
+
+    config.storage = :fog
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+  else
+    config.storage = :file
   end
 end
